e14773f59917074309eb3e2c415655fbf4c178ff
Marcin <marcinburek16@email.com>
2026-03-31 23:31:32 +0200

    prototyping modals and overlays

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const new_note_button = document.getElementById("newNoteButton")
+const edit_note_button = document.getElementById("editNoteButton")
+const modal_overlay = document.getElementById("modalOverlay")
+const modal1 = document.getElementById("modal1")
 const section = document.getElementById("section")
 
+
 new_note_button.addEventListener("click", () => {
+    modal_overlay.classList.remove("Hidden");
+    modal1.classList.remove("Hidden")
+
     const note = document.createElement("div")
     note.classList.add("note")
 
