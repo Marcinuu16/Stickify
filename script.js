cb33dd3f969a889e7df20f706384adb566f0de58
Marcin <marcinburek16@email.com>
2026-04-02 00:04:20 +0200

    Modale 2.0

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-const new_note_button = document.getElementById("newNoteButton")
-const edit_note_button = document.getElementById("editNoteButton")
-const modal_overlay = document.getElementById("modalOverlay")
-const modal1 = document.getElementById("modal1")
-const section = document.getElementById("section")
+const section = document.getElementById("section");
+const modal_overlay = document.getElementById("modal_overlay");
 
+const new_note_button = document.getElementById("new_note_button");
+const new_note_modal = document.getElementById("new_note_modal");
+
+const edit_note_button = document.getElementById("edit_note_button");
+const edit_note_modal = document.getElementById("edit_note_modal");
 
 new_note_button.addEventListener("click", () => {
-    modal_overlay.classList.remove("Hidden");
-    modal1.classList.remove("Hidden")
+    modal_overlay.classList.remove("hidden");
+    new_note_modal.classList.remove("hidden");
 
-    const note = document.createElement("div")
-    note.classList.add("note")
+    create_note();
+});
+new_note_modal.querySelector(".close_modal").addEventListener("click", close_new_note_modal)
+modal_overlay.addEventListener("click", close_new_note_modal);
 
-    const img = document.createElement("img")
-    img.src = "wordnote.png"
-    img.alt = ""
 
-    note.appendChild(img)
-    section.appendChild(note)
-    console.log('dodano!')
+edit_note_button.addEventListener("click", () => {
+    modal_overlay.classList.remove("hidden");
+    edit_note_modal.classList.remove("hidden");
 })
+edit_note_modal.querySelector(".close_modal").addEventListener("click", close_edit_note_modal)
+modal_overlay.addEventListener("click", close_edit_note_modal);
+
+
+function close_new_note_modal() {
+    modal_overlay.classList.add("hidden");
+    new_note_modal.classList.add("hidden");
+}
+
+function close_edit_note_modal() {
+    modal_overlay.classList.add("hidden");
+    edit_note_modal.classList.add("hidden");
+}
+
+function create_note(){
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    const img = document.createElement("img");
+    img.src = "wordnote.png";
+
+    note.appendChild(img);
+    section.appendChild(note);
+
+    console.log("dodano!");
+}
+
