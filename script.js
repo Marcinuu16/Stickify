cf23a7c15fd4bec2c79c03e02083360b74ca7eb5
Marcin <marcinburek16@email.com>
2026-04-02 23:45:20 +0200

    basic note system

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 const section = document.getElementById("section");
 const modal_overlay = document.getElementById("modal_overlay");
-
 const new_note_button = document.getElementById("new_note_button");
 const new_note_modal = document.getElementById("new_note_modal");
-
+const create_note_title = document.getElementById("create_note_title")
+const create_note_text = document.getElementById("create_note_text")
+const create_note_color = document.getElementById("create_note_color")
+const create_note_button = document.getElementById("create_note_button")
 const edit_note_button = document.getElementById("edit_note_button");
 const edit_note_modal = document.getElementById("edit_note_modal");
+
+
+
+
+
 
 new_note_button.addEventListener("click", () => {
     modal_overlay.classList.remove("hidden");
     new_note_modal.classList.remove("hidden");
 
-    create_note();
+
 });
 new_note_modal.querySelector(".close_modal").addEventListener("click", close_new_note_modal)
 modal_overlay.addEventListener("click", close_new_note_modal);
+
+create_note_button.addEventListener("click", () => {
+
+    if(!validate_note_creation()){
+        console.log("brakuje!");
+    }
+    else{
+        create_note();
+    }
+    
+    close_new_note_modal();
+});
+
+
+
+
 
 
 edit_note_button.addEventListener("click", () => {
@@ -23,6 +46,13 @@ edit_note_button.addEventListener("click", () => {
 })
 edit_note_modal.querySelector(".close_modal").addEventListener("click", close_edit_note_modal)
 modal_overlay.addEventListener("click", close_edit_note_modal);
+
+
+
+
+
+
+
 
 
 function close_new_note_modal() {
@@ -35,14 +65,30 @@ function close_edit_note_modal() {
     edit_note_modal.classList.add("hidden");
 }
 
-function create_note(){
+function validate_note_creation() {
+    const title = create_note_title.value.trim();
+    const text = create_note_text.value.trim();
+    const color = create_note_color.value;
+
+    if (!title) return false;
+    if (!text) return false;
+    if (!color) return false;
+
+    return true;
+}
+
+function create_note() {
     const note = document.createElement("div");
     note.classList.add("note");
 
-    const img = document.createElement("img");
-    img.src = "wordnote.png";
-
-    note.appendChild(img);
+    const title = document.createElement("h3")
+    title.textContent = create_note_title.value.trim();
+    note.appendChild(title)
+    const text = document.createElement("p");
+    text.textContent = create_note_text.value.trim();
+    note.appendChild(text)
+    note.style.backgroundColor = create_note_color.value;
+    
     section.appendChild(note);
 
     console.log("dodano!");
