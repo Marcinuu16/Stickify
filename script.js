c865276ac5b1bcdf6be85bd3e1f9504ac3d3e9d5
Marcin <marcinburek16@email.com>
2026-04-04 23:48:23 +0200

    deletion 1.0

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ const create_note_color = document.getElementById("create_note_color")
 const create_note_button = document.getElementById("create_note_button")
 const edit_note_button = document.getElementById("edit_note_button");
 const edit_note_modal = document.getElementById("edit_note_modal");
+const delete_note_button = document.getElementById("delete_note_button");
+const delete_note_modal = document.getElementById("delete_note_modal");
 
 let notes = []
-var marked_ids = []
+let is_delete_mode = false
+let notes_in_DOM;
 
 load_notes();
 show_notes();
@@ -18,11 +21,13 @@ show_notes();
 new_note_button.addEventListener("click", () => {
     modal_overlay.classList.remove("hidden");
     new_note_modal.classList.remove("hidden");
-
+    delete_mode(false)
 
 });
 new_note_modal.querySelector(".close_modal").addEventListener("click", close_new_note_modal)
 modal_overlay.addEventListener("click", close_new_note_modal);
+
+
 
 create_note_button.addEventListener("click", () => {
     const data = get_note_form_data();
@@ -38,9 +43,11 @@ create_note_button.addEventListener("click", () => {
 });
 
 
+
 edit_note_button.addEventListener("click", () => {
     modal_overlay.classList.remove("hidden");
     edit_note_modal.classList.remove("hidden");
+    delete_mode(false)
     console.log(notes);
 
 })
@@ -48,6 +55,12 @@ edit_note_modal.querySelector(".close_modal").addEventListener("click", close_ed
 modal_overlay.addEventListener("click", close_edit_note_modal);
 
 
+
+delete_note_button.addEventListener("click", () => {
+    toggle_delete_mode()
+})
+delete_note_modal.querySelector(".close_modal")
+    .addEventListener("click", () => delete_mode(false));
 
 
 
@@ -65,12 +78,14 @@ function close_edit_note_modal() {
     edit_note_modal.classList.add("hidden");
 }
 
-function show_notes() {
-    notes.forEach(note_item => {
-        if (marked_ids.includes(note_item.id)) {
-            return; 
-        }
+function close_delete_note_modal() {
+    delete_note_modal.classList.add("hidden");
+}
 
+function show_notes() {
+    section.innerHTML = "";
+
+    notes.forEach(note_item => {
         const note = document.createElement("div");
         note.classList.add("note");
 
@@ -79,19 +94,50 @@ function show_notes() {
         note.appendChild(title);
 
         const text = document.createElement("p");
-        text.textContent = note_item.text.slice(0,40)
+        text.textContent = note_item.text.slice(0, 40);
         note.appendChild(text);
 
         note.style.backgroundColor = note_item.bgcolor;
 
         section.appendChild(note);
-
-        console.log("dodano!");
-        marked_ids.push(note_item.id); 
     });
 }
 
 
+function toggle_delete_mode() {
+    if (is_delete_mode == true) {
+        delete_mode(false)
+    }
+    else {
+        delete_mode(true)
+    }
+}
+
+function delete_mode(value) {
+    is_delete_mode = value;
+
+    if (value == true) {
+        document.body.style.cursor = 'url("delete_cursor.png"), auto';
+        delete_note_button.style.backgroundColor = "#652c2c"
+        gather_notes_DOM()
+        notes_in_DOM.forEach(note_dom => {  
+            note_dom.classList.add("deletable")
+        })
+
+        delete_note_modal.classList.remove("hidden")
+        console.log("delete mode on")
+    } else {
+        document.body.style.cursor = 'auto';
+        delete_note_button.style.backgroundColor = "rgba(31, 6, 56, 1)";
+        gather_notes_DOM()
+        notes_in_DOM.forEach(note_dom => {  
+            note_dom.classList.remove("deletable")
+        })
+
+        console.log("delete mode off")
+        close_delete_note_modal()
+    }
+}
 
 function get_note_form_data() {
     return {
@@ -123,20 +169,23 @@ function create_note(data) {
     save_notes();
 }
 
+function gather_notes_DOM(){
+    notes_in_DOM = section.querySelectorAll(".note");
+}
 
-function load_notes(){
+function load_notes() {
     const saved = localStorage.getItem("notes");
 
-    if(saved){
+    if (saved) {
         notes = JSON.parse(saved)
     }
-    else{
+    else {
         notes = []
     }
 }
 
-function save_notes(){
-    localStorage.setItem("notes",JSON.stringify(notes));
+function save_notes() {
+    localStorage.setItem("notes", JSON.stringify(notes));
 }
 
 
